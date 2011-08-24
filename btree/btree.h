3097e51e9d6774b381be5eca4c519b509cb07309
Derick Rethans <github@derickrethans.nl>
2011-08-24 15:00:19 +0100

    Put back the structs.

--- btree/btree.h
+++ btree/btree.h
@@ -1,3 +1,24 @@
+#define BTREE_T    102
+#define BTREE_T2   204
+
+typedef struct {
+	uint64_t key;
+	uint32_t idx;       /* index into data portion */
+	uint32_t expire_ts; /* if 0, then not in use */
+} btree_key; /* 16 bytes */
+
+typedef struct {
+	uint32_t      idx;        /* the node's index into the data store */
+	uint32_t      branch[BTREE_T2];
+	btree_key     keys[BTREE_T2 - 1];
+	char          leaf;       /* whether it's a leaf node or not */
+	uint16_t      nr_of_keys; /* number of keys in use */
+} btree_node; /* 204*4 + 203*16 + 1 + 2 = 4067, which fits nicely in a page */
+
+typedef struct {
+	btree_node *root;
+} btree;
+
 int btree_search(btree_node *node, uint64_t key, uint32_t *idx);
 btree_tree *btree_allocate(uint32_t nr_of_items, uint32_t data_size);
 btree_tree *btree_create(void);
