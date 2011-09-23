6a9f211fb182a593e33ea8f1d5751dfb5e1113fe
Derick Rethans <github@derickrethans.nl>
2011-09-23 10:47:44 +0100

    Fixed speling.

--- btree/test-programs/tree-delete2.c
+++ btree/test-programs/tree-delete2.c
@@ -35,9 +35,24 @@ int main(void)
 	btree_delete(tmp, 'E');
 	btree_dump(tmp);
 
-	/* Delete internal node (the root node in this case, but nowpreviousfirst child and next child have less than N keys) */
-	btree_delete(tmp, 'P');
+	/* Delete internal node (the root node in this case, but now the previous child and next child have less than N keys) */
+	btree_delete(tmp, 'P'); /* Left: ABQZ */
 	btree_dump(tmp);
+
+	/* Delete node so that the root node becomes one sized */
+	btree_delete(tmp, 'A');
+	btree_delete(tmp, 'B');
+	btree_delete(tmp, 'Z');
+	btree_dump(tmp);
+
+	/* Delete last node */
+	btree_delete(tmp, 'Q');
+	btree_dump(tmp);
+
+	/* Add one more just to see if that works */
+	btree_insert(tmp, 'W', &data_idx); printf("%u\n", data_idx);
+	btree_dump(tmp);
+
 	btree_free(tmp);
 
 	return 0;
